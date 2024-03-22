c5718262879949cd95df478d2cac790dfdbdcb3f
zainbabar <zain.babar102@gmail.com>
2024-03-22 15:47:28 -0400

    ccc

--- DecisionStructures/IfElseIsStatements/TriangleTimes.java
+++ DecisionStructures/IfElseIsStatements/TriangleTimes.java
@@ -1,30 +1,29 @@
+// CCC practice problem
+
 import java.util.Scanner;
 
-public class TriangleTImes {
+public class TriangleTimes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the first angle:");
         double angle1 = scanner.nextInt();
 
-        System.out.println("Enter the second angle:");
         double angle2 = scanner.nextInt();
 
-        System.out.println("Enter the third angle:");
         double angle3 = scanner.nextInt();
 
         scanner.close();
 
         double angleSum = angle1 + angle2 + angle3;
 
-        if (angleSum > 180){
-            System.out.println("Error.");
+        if (angleSum != 180){
+            System.out.println("Error");
         } else if (angle1 == angle2 && angle2 == angle3){
-            System.out.println("The triangle is equaliateral.");
-        } else if (angle1 == angle2 || angle2 == angle3 ){
-            System.out.println("The triangle is isosceles.");
-        }else if (angle1 != angle2 && angle2 != angle3){
-            System.out.println("The triangle is scalene.");
+            System.out.println("Equilateral");
+        } else if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3){
+            System.out.println("Isosceles");
+        } else {
+            System.out.println("Scalene");
         }
     }
 }
