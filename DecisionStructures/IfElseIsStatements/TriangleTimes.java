ae7ffa1df85fed3aa04768bf7cdc629f76ff8af8
zainbabar <zain.babar102@gmail.com>
2024-03-22 00:42:40 -0400

    edit

--- DecisionStructures/IfElseIsStatements/TriangleTimes.java
+++ DecisionStructures/IfElseIsStatements/TriangleTimes.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
 
-public class TriangleTimes {
+public class TriangleTImes {
     public static void main(String[] args) {
-        System.out.println("hello");
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first angle:");
+        double angle1 = scanner.nextInt();
+
+        System.out.println("Enter the second angle:");
+        double angle2 = scanner.nextInt();
+
+        System.out.println("Enter the third angle:");
+        double angle3 = scanner.nextInt();
+
+        scanner.close();
+
+        double angleSum = angle1 + angle2 + angle3;
+
+        if (angleSum > 180){
+            System.out.println("Error.");
+        } else if (angle1 == angle2 && angle2 == angle3){
+            System.out.println("The triangle is equaliateral.");
+        } else if (angle1 == angle2 || angle2 == angle3 ){
+            System.out.println("The triangle is isosceles.");
+        }else if (angle1 != angle2 && angle2 != angle3){
+            System.out.println("The triangle is scalene.");
+        }
     }
 }
