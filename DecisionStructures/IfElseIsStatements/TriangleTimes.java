74fac2f40478cefff8a1b3e52884e3691c97aed2
zainbabar <zain.babar102@gmail.com>
2024-03-24 19:20:05 -0400

    edit

--- DecisionStructures/IfElseIsStatements/TriangleTimes.java
+++ DecisionStructures/IfElseIsStatements/TriangleTimes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TriangleTImes {
+public class TriangleTimes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
