e064e1b3ca827696be96add875bacd54a2e1c5dc
zainbabar <zain.babar102@gmail.com>
2024-04-06 14:51:44 -0400

    e

--- DecisionStructures/Review/CakeProblemReview.java
+++ DecisionStructures/Review/CakeProblemReview.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class CakeProblemReview {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int cakes = 0;
         
         System.out.println("Enter the number of eggs: ");
         int eggs = scan.nextInt() / 3;
@@ -12,18 +14,20 @@ public class CakeProblemReview {
 
         System.out.println("Enter the amount of milk: ");
         int milk = scan.nextInt();
-        
-        if (eggs < flour && eggs < milk){
-            System.out.println("You can make " + eggs + " cake(s)");
+
+        scan.close();
+
+        if (eggs < 3 || flour < 2 || milk < 1){
+            System.out.println("You can make 0 cake(s).");
+        } else if (eggs < flour && eggs < milk){
+            cakes = eggs;
+        } else  if (milk < flour && milk < eggs){
+            cakes = milk;
+        } else if (flour < milk && flour < eggs){
+            cakes = flour;
+        } else {  
+            cakes = eggs;
         }
-        else  if (milk < flour && milk < eggs){
-            System.out.println("You can make " + milk + " cake(s)");
-        }
-        else if (flour < milk && flour < eggs){
-            System.out.println("You can make " + flour + " cake(s)");
-        }
-        else {
-            System.out.println("You can make " + eggs + " cake(s)");
-        }       
-    }       
+        System.out.println("You can make " + cakes + " cake(s)");
+    }
 }
---
+++ DecisionStructures/Review/TeenSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TeenSum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int a = scan.nextInt();
+
+        System.out.println("Enter the second number: ");
+        int b = scan.nextInt();
+
+        int c = a + b;
+
+        if (a < 20 && a > 12 || b < 20 && b > 12 || c < 20 && c > 12){
+            System.out.println("19");
+        }
+        else {
+            System.out.println(c);
+        }
+    }
+}
