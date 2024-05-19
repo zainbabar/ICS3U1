079ed8a6ce23ac3250e5965b69a7471c64e01a27
zain <zain.babar102@gmail.com>
2024-05-19 19:47:22 -0400

    Create Numbers.java

---
+++ Java/ClassesMethods/loops/Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter an int");
+        int a = scan.nextInt();
+        
+        System.out.println("enter an int");
+        int b = scan.nextInt(`);
+        
+    }
+}
