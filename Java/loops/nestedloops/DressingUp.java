f0ba115dcc993a5efc44814261d9ae7f3043c6d6
zain <zain.babar102@gmail.com>
2024-06-12 16:38:03 -0400

    e

--- Java/loops/nestedloops/DressingUp.java
+++ Java/loops/nestedloops/DressingUp.java
@@ -2,19 +2,44 @@ import java.util.Scanner;
 
 public class DressingUp {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("enter a height");
-        int h = scan.nextInt();
-        DressingUp obj = new DressingUp();
-        obj.draw(h);
-    }
-    public void draw(int h){
-        for (int i = 0; i < h / 2; i++){
-            for (int j = 0; j < i; j++){
-                System.out.print("*");
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for height of the bowtie
+        System.out.print("Enter the height of the bowtie (odd number): ");
+        int height = scanner.nextInt();
+
+        // Ensure height is an odd number
+        if (height % 2 == 0) {
+            System.out.println("Height must be an odd number.");
+            return;
+        }
+
+        int mid = height / 2;
+
+        // Upper part of the bowtie
+        for (int i = 0; i <= mid; i++) {
+            for (int j = 0; j < height; j++) {
+                if (j < i || j >= height - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
-        
+
+        // Lower part of the bowtie
+        for (int i = mid - 1; i >= 0; i--) {
+            for (int j = 0; j < height; j++) {
+                if (j < i || j >= height - i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
